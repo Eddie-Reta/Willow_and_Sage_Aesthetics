6bdeaf1f3f684a7ae8371169dfaf25a0e2184925
Eduardo Reta Jr <https://github.com/Eddie-Reta>
2022-06-22 06:43:46 -0400

    update on css

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,7 +1,4 @@
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import {Map, View, TileLayer, Osm} from 'ol/Map';
 
 $(function() {
     const map = new Map({
