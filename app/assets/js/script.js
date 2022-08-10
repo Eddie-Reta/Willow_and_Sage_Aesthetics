ada5c41d54848745998a50a498c27061cfac1cf5
Eduardo Reta Jr <dev.eddie.reta@gmail.com>
2022-08-09 21:14:35 -0400

    Booking button Service has Link

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,6 +1,6 @@
 //import {Map, View, TileLayer, Osm} from 'ol/Map';
 
-$(function() {
+// $(function() {
     // const map = new Map({
     //     view: new View({
     //       center: [0, 0],
@@ -21,11 +21,11 @@ $(function() {
     //   document.getElementById("navbar").style.padding = "80px 10px";
     //   document.getElementById("logo").style.fontSize = "35px";
     // }
-    window.onscroll = () => {scrollFunction()};
-    const scrollFunction = () => {
-      const windowScroll = $("<body>").topScroll();
-      if (windowScroll > 80 || windowScroll > 80) {
-        $("<nav>").css("padding: 10vh")
-      }
-    };
-})
+//     window.onscroll = () => {scrollFunction()};
+//     const scrollFunction = () => {
+//       const windowScroll = $("<body>").topScroll();
+//       if (windowScroll > 80 || windowScroll > 80) {
+//         $("<nav>").css("padding: 10vh")
+//       }
+//     };
+// })
